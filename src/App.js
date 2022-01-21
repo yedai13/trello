dd845e3632012a1d4c193ced6dc1ac8059f7cd9f
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-21 09:52:23 -0300

    build trello list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from "@material-ui/core";
+import "./App.css";
+import TrelloList from "./components/TrelloList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <TrelloList />
     </div>
   );
 }
---
+++ src/components/TrelloList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper } from "@material-ui/core";
+
+const TrelloList = () => {
+  return (
+    <Paper>
+      <h1>Trello List</h1>
+    </Paper>
+  );
+};
+
+export default TrelloList;
