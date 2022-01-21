a223d0ee918e839f267b213038050dd147f7e495
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-21 10:17:52 -0300

    add styles to trello list

--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -3,10 +3,20 @@ import { Paper } from "@material-ui/core";
 import ListTitle from "./ListTitle";
 import TrelloCard from "./TrelloCard";
 import AddCardorList from "./AddCardOrList";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: 300,
+    backgroundColor: "#EBECF0",
+    margin: theme.spacing(2),
+  },
+}));
 
 const TrelloList = () => {
+  const { paper } = useStyles();
   return (
-    <Paper>
+    <Paper className={paper}>
       <ListTitle />
       <TrelloCard />
       <AddCardorList />
