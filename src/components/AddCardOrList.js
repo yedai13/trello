404d97b8630b8521d1c9b9b5f323c9f7552d6bbc
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-21 11:39:27 -0300

    add style to button add card

--- src/components/AddCardOrList.js
+++ src/components/AddCardOrList.js
@@ -1,21 +1,41 @@
-import { Collapse, Paper, Typography } from "@material-ui/core";
+import { Box, Collapse, Paper, Typography } from "@material-ui/core";
 import React, { useState } from "react";
 import AddCardOrListText from "./AddCardOrListText";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  collapseContainer: {},
+  addCardOrListText: {
+    backgroundColor: "#EBECF0",
+    boxShadow: "none",
+    margin: theme.spacing(0, 1, 1, 1),
+    padding: theme.spacing(1),
+    color: "#5E6C84",
+    "&:hover": {
+      // backgroundColor:  fade("#000", 0.25)
+      backgroundColor: "#dadce2",
+      color: "#172b4d",
+      cursor: "pointer",
+    },
+  },
+}));
 
 const AddCardorList = () => {
   const [open, setOpen] = useState(false);
+
+  const { addCardOrListText, collapseContainer } = useStyles();
   return (
-    <div>
+    <Box className={collapseContainer}>
       <Collapse in={open}>
         <AddCardOrListText />
       </Collapse>
 
       <Collapse in={!open}>
-        <Paper>
-          <Typography>+ Add a card</Typography>
+        <Paper className={addCardOrListText}>
+          <Typography variant="body2">+ Add a card</Typography>
         </Paper>
       </Collapse>
-    </div>
+    </Box>
   );
 };
 
