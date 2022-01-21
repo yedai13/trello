2362c93b7ef487e0d27a9f497437a6900bbada4d
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-21 10:00:46 -0300

    build add card

---
+++ src/components/AddCardOrList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCardorList = () => {
+  return (
+    <div>
+      <h2>add card</h2>
+    </div>
+  );
+};
+
+export default AddCardorList;
--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 import ListTitle from "./ListTitle";
+import TrelloCard from "./TrelloCard";
+import AddCardorList from "./AddCardOrList";
 
 const TrelloList = () => {
   return (
     <Paper>
       <ListTitle />
+      <TrelloCard />
+      <AddCardorList />
     </Paper>
   );
 };
